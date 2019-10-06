08c01f31d1979205821265bd66e85cfa092c0679
Ulrich Schmidt <u.sch.zw@gmx.de>
2019-10-06 10:05:51 +0200

    module auto loader
    loads all modules in alphabetical order
    returns: Engine

--- engine/init.lua
+++ engine/init.lua
@@ -1,5 +1,14 @@
-local path = ... .. "."
 
-require (path .. "reader")
+return (function (path)
+  local noAutoLoad = "SKELETON.lua init.lua engine.lua";
+  local filelist = {};
+  for _, fn in ipairs(love.filesystem.getDirectoryItems(path:gsub("%.","/"))) do
+    if fn:find(".lua$") and not noAutoLoad:find(fn) then
+      table.insert(filelist, path.."."..fn:sub(1, -5));
+    end;
+  end;
+  table.sort(filelist);
+  for _, m in ipairs(filelist) do  require(m); end;
+  return require (path .. ".engine");
+end)(...)
 
-return require (path .. "engine")
