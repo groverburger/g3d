8719621665fa575196f60e71d48864f6a8841f3a
groverburger <zachary.m.booth@gmail.com>
2021-11-02 12:18:59 -0700

    simplified objloader

--- g3d/objloader.lua
+++ g3d/objloader.lua
@@ -6,120 +6,62 @@
 -- simple obj loader
 ----------------------------------------------------------------------------------------------------
 
--- stitch two tables together and return the result
-local function concatTables(t1,t2,t3)
-    local ret = {}
-
-    for i,v in ipairs(t1) do
-        ret[#ret +1] = v
-    end
-    for i,v in ipairs(t2) do
-        ret[#ret +1] = v
-    end
-    for i,v in ipairs(t3) do
-        ret[#ret +1] = v
-    end
-
-    return ret
-end
-
 -- give path of file
 -- returns a lua table representation
-local function objLoader(path)
-    local verts = {}
-    local faces = {}
-    local uvs = {}
-    local normals = {}
+return function (path)
+    local positions, faces, uvs, normals, model = {}, {}, {}, {}, {}
 
     -- go line by line through the file
     for line in love.filesystem.lines(path) do
         local words = {}
 
         -- split the line into words
-        for word in line:gmatch("([^".."%s".."]+)") do
+        for word in line:gmatch "([^%s]+)" do
             table.insert(words, word)
         end
 
-        -- if the first word in this line is a "v", then this defines a vertex
-        if words[1] == "v" then
-            verts[#verts+1] = {tonumber(words[2]), tonumber(words[3]), tonumber(words[4])}
-        end
+        local firstWord = words[1]
 
-        -- if the first word in this line is a "vt", then this defines a texture coordinate
-        if words[1] == "vt" then
-            uvs[#uvs+1] = {tonumber(words[2]), tonumber(words[3])}
-        end
+        if firstWord == "v" then
+            -- if the first word in this line is a "v", then this defines a vertex's position
 
-        -- if the first word in this line is a "vn", then this defines a vertex normal
-        if words[1] == "vn" then
-            normals[#normals+1] = {tonumber(words[2]), tonumber(words[3]), tonumber(words[4])}
-        end
+            table.insert(positions, {tonumber(words[2]), tonumber(words[3]), tonumber(words[4])})
+        elseif firstWord == "vt" then
+            -- if the first word in this line is a "vt", then this defines a texture coordinate
 
-        -- if the first word in this line is a "f", then this is a face
-        -- a face takes three arguments which refer to points, each of those points take three arguments
-        -- the arguments a point takes is v,vt,vn
-        if words[1] == "f" then
-            local store = {}
+            table.insert(uvs, {tonumber(words[2]), tonumber(words[3])})
+        elseif firstWord == "vn" then
+            -- if the first word in this line is a "vn", then this defines a vertex normal
 
-            assert(#words == 4, "Faces in "..path.." must be triangulated before they can be used in g3d!")
+            table.insert(normals, {tonumber(words[2]), tonumber(words[3]), tonumber(words[4])})
+        elseif firstWord == "f" then
+            -- if the first word in this line is a "f", then this is a face
+            -- a face takes three point definitions
+            -- the arguments a point definition takes are vertex, vertex texture, vertex normal in that order
+
+            assert(#words == 4, ("Faces in %s must be triangulated before they can be used in g3d!"):format(path))
+            local face = {v = {}, vt = {}, n = {}}
 
             for i=2, #words do
-                local num = ""
-                local word = words[i]
-                local ii = 1
-                local char = word:sub(ii,ii)
-
-                while true do
-                    char = word:sub(ii,ii)
-                    if char ~= "/" then
-                        num = num .. char
-                    else
-                        break
-                    end
-                    ii = ii + 1
-                end
-                store[#store+1] = tonumber(num)
-
-                local num = ""
-                ii = ii + 1
-                while true do
-                    char = word:sub(ii,ii)
-                    if ii <= #word and char ~= "/" then
-                        num = num .. char
-                    else
-                        break
-                    end
-                    ii = ii + 1
-                end
-                store[#store+1] = tonumber(num)
-
-                local num = ""
-                ii = ii + 1
-                while true do
-                    char = word:sub(ii,ii)
-                    if ii <= #word and char ~= "/" then
-                        num = num .. char
-                    else
-                        break
-                    end
-                    ii = ii + 1
-                end
-                store[#store+1] = tonumber(num)
+                local v, vt, n = words[i]:match "(%d+)/(%d+)/(%d+)"
+                table.insert(face.v,  tonumber(v))
+                table.insert(face.vt, tonumber(vt))
+                table.insert(face.n,  tonumber(n))
             end
 
-            faces[#faces+1] = store
+            table.insert(faces, face)
         end
     end
 
     -- put it all together in the right order
-    local compiled = {}
-    for i,face in pairs(faces) do
-        compiled[#compiled +1] = concatTables(verts[face[1]], uvs[face[2]], normals[face[3]])
-        compiled[#compiled +1] = concatTables(verts[face[4]], uvs[face[5]], normals[face[6]])
-        compiled[#compiled +1] = concatTables(verts[face[7]], uvs[face[8]], normals[face[9]])
+    for _, face in pairs(faces) do
+        for i=1, 3 do
+            local vert = {unpack(positions[face.v[i]])}
+            for _, uv in ipairs(uvs[face.vt[i]]) do table.insert(vert, uv) end
+            for _, normal in ipairs(normals[face.n[i]]) do table.insert(vert, normal) end
+            table.insert(model, vert)
+        end
     end
 
-    return compiled
+    return model
 end
-
-return objLoader
